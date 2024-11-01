f3b339acc3c5a6e77e58c4f788cab91730c2b7c4
Shehzad-Rahim <shehzadrahim33@gmail.com>
2024-11-01 20:11:05 +0500

    class07 assignment 1

---
+++ app/tailwindGrid/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1 className="text-2xl text-center m-2 font-bold">Tailwind Css Grid</h1>
+    </div>
+  )
+}
+
+export default page
